74261eb1bee69ee1c51857aad1eb48e5d4308b81
Nattapong Ekudomsuk <nuttapong_mos@hotmail.com>
2026-09-06 16:54:54 +0800

    alertmanager: fix test case
    add url scheme

--- pkg/alertmanager/validation/v1beta1/validation_test.go
+++ pkg/alertmanager/validation/v1beta1/validation_test.go
@@ -723,7 +723,7 @@ func TestValidateOpsGenieAlertmanagerConfig(t *testing.T) {
 							Name: "different",
 							OpsGenieConfigs: []monitoringv1beta1.OpsGenieConfig{
 								{
-									APIURL: ptr.To(monitoringv1beta1.URL("www.test.com")),
+									APIURL: ptr.To(monitoringv1beta1.URL("https://www.test.com")),
 								},
 							},
 						},
